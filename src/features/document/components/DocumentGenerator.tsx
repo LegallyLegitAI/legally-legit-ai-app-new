f49be3b590864ee6e952a9bb9fa35990eb44b3dc
Legally Legit AI <admin@legallylegitai.com.au>
2025-08-06 07:24:38 +1000

    j

--- src/features/document/components/DocumentGenerator.tsx
+++ src/features/document/components/DocumentGenerator.tsx
@@ -1,24 +1,7 @@
-import { Container, Form, Button } from 'react-bootstrap';
+import DocumentGeneratorWizard from './DocumentGeneratorWizard';
 
 const DocumentGenerator = () => {
-  return (
-    <Container className="mt-5">
-      <h1>Document Generator</h1>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Select Document Type</Form.Label>
-          <Form.Select>
-            <option>Independent Contractor Agreement</option>
-            <option>Non-Disclosure Agreement (NDA)</option>
-            <option>Website Terms of Use</option>
-          </Form.Select>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Generate Document
-        </Button>
-      </Form>
-    </Container>
-  );
+  return <DocumentGeneratorWizard />;
 };
 
 export default DocumentGenerator;
